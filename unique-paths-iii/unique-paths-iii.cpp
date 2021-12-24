28aaadb277f9531a595119cc3668e068f4b54a08
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2021-12-24 23:03:52 +0530

    Time: 4 ms (73.15%), Space: 7.1 MB (25.87%) - LeetHub

--- unique-paths-iii/unique-paths-iii.cpp
+++ unique-paths-iii/unique-paths-iii.cpp
@@ -1,3 +1,6 @@
+// time com-> har cell ke pass 3 options hai but 1st ke pass 4 hai to  4*3*3*3....=3^(m+n)
+//     space-m*n coz space is equal to the height of stack and in worst case stack can have all elements so m*n
+
 class Solution {
 public:
    typedef pair<int,int> Pair;
@@ -21,17 +24,16 @@ public:
       
         grid[starti][startj]=-1;
         initialcount--;
-             // int path1=solution(starti,startj+1,grid,initialcount);
-             // int path2=solution(starti+1,startj,grid,initialcount);
-             // int path3=solution(starti,startj-1,grid,initialcount);
-             // int path4=solution(starti-1,startj,grid,initialcount);
-int p=solution(starti,startj+1,grid,initialcount)+solution(starti+1,startj,grid,initialcount)+
-    solution(starti,startj-1,grid,initialcount)+solution(starti-1,startj,grid,initialcount);
+             int path1=solution(starti,startj+1,grid,initialcount);
+             int path2=solution(starti+1,startj,grid,initialcount);
+             int path3=solution(starti,startj-1,grid,initialcount);
+             int path4=solution(starti-1,startj,grid,initialcount);
+
         initialcount++;
         grid[starti][startj]=0;
       
-     // int ans= path1+path2+path3+path4;
-   return p;
+     int ans= path1+path2+path3+path4;
+   return ans;
     }
     
     int uniquePathsIII(vector<vector<int>>& grid) {
