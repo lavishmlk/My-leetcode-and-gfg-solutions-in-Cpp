09cd0ac0ff40aeec41922f2624e13436170cdc15
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2021-12-24 21:37:10 +0530

    Time: 4 ms (73.15%), Space: 7.1 MB (57.40%) - LeetHub

---
+++ unique-paths-iii/unique-paths-iii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+   typedef pair<int,int> Pair;
+
+  int solution(int starti,int startj,vector<vector<int>>& grid,int initialcount){
+   
+      
+         if(starti<0 || startj<0 || starti>=grid.size() || startj>=grid[0].size() || grid[starti][startj]==-1){
+            return 0;
+        }
+      
+        if(grid[starti][startj]==2){
+            if( initialcount==-1)
+          return 1;
+            else
+                return 0;
+        }
+        
+     
+        
+      
+        grid[starti][startj]=-1;
+        initialcount--;
+             // int path1=solution(starti,startj+1,grid,initialcount);
+             // int path2=solution(starti+1,startj,grid,initialcount);
+             // int path3=solution(starti,startj-1,grid,initialcount);
+             // int path4=solution(starti-1,startj,grid,initialcount);
+int p=solution(starti,startj+1,grid,initialcount)+solution(starti+1,startj,grid,initialcount)+
+    solution(starti,startj-1,grid,initialcount)+solution(starti-1,startj,grid,initialcount);
+        initialcount++;
+        grid[starti][startj]=0;
+      
+     // int ans= path1+path2+path3+path4;
+   return p;
+    }
+    
+    int uniquePathsIII(vector<vector<int>>& grid) {
+     
+        
+        
+             int count=0;
+        int starti=0;
+            int startj=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==0){
+                    count++;
+                }
+               else if(grid[i][j]==1){
+                    starti=i;
+                    startj=j;
+                }
+               
+           
+            }  
+        }
+      return  solution(starti,startj,grid,count);
+    }
+};
