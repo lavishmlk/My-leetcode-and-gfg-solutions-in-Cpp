92b778359fb59b0f4df9ab706846654163c1ddba
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-01-20 15:48:22 +0530

    Time: 20 ms (54.02%), Space: 10.7 MB (56.56%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,21 +4,21 @@ public:
        
         vector<int>arr;
         unordered_map<int,int>m;
-        m.insert({nums[0],0});
         
-        for(int i=1;i<nums.size();i++){
+        
+        for(int i=0;i<nums.size();i++){
             
             auto it=m.find(target-nums[i]);
             if(it!=m.end()){
                 arr.push_back(it->second);
                 arr.push_back(i);
-                break;
+               return arr;
             }
                else{
                    m.insert({nums[i],i});
                }
         
     }
-        return arr;
+       return arr; 
     }
 };
