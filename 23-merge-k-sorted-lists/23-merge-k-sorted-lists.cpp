6a89d0ca9cf4b7b343231f741693dc7ca4e50671
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-02-09 11:56:18 +0530

    Time: 676 ms (6.76%), Space: 13 MB (89.21%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,43 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-  
-     struct compare
-    {
-        bool operator()(ListNode* &a,ListNode* &b)
-        {
-            return a->val>b->val;
+  //Brute force O(N*K^2)
+    int minidx(vector<ListNode*>& lists){
+         int Min=INT_MAX;
+  int idx=-1;
+        for(int i=0;i<lists.size();i++){
+             if(lists[i]!=NULL && lists[i]->val<Min){
+                Min=lists[i]->val;
+                 idx=i;
+            }
         }
-    };
+        return idx;
+    }
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,compare>pq;
-
-          for(ListNode* x:lists){
-              if(x!=NULL)
-              pq.push(x);
-          }   
-                    ListNode* head=NULL;
-            ListNode* tail=NULL;
-        
-        while(!pq.empty()){
-            
-            ListNode* val=pq.top();
-            pq.pop();
-            
-            if(head==NULL){
-                // head=val;
-                tail=val;
-                head=tail;
-            }
-            else{
-                tail->next=val;
-                tail=val;   
-            }
-            
-if(val->next){
-    pq.push(val->next);
-}
+        if(lists.size()==0){
+            return NULL;
         }
-        return head;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* head=dummy;
+        ListNode* tail=dummy;
+      
+        while(true){
+            
+             int Minidx=minidx(lists); 
+            if(Minidx==-1){
+                break;
+            }
+            tail->next=lists[Minidx];
+            tail=lists[Minidx];
+            lists[Minidx]=lists[Minidx]->next;
+        }
+        return dummy->next;
         
-}
+    }
 };
