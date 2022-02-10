1e7815344d0de85ac8d436afc8232a2299f4a634
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-02-11 03:14:53 +0530

    Time: 21 ms (23.52%), Space: 8.1 MB (58.18%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -8,8 +8,14 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        
+    bool hasCycle(ListNode *head){
+        //agar ye dono if wle corner cases nhi likhte tab bhi kaam chal jaata
+        if(head==NULL || head->next==NULL){
+            return false;
+        }
+        if(head->next==head){
+            return true;
+        }
         ListNode* slow=head;
         ListNode* fast=head;
         while(fast!=NULL && fast->next!=NULL){
