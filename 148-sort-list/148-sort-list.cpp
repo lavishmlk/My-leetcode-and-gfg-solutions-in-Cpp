60c3862647d339a96955b00217877ba7ca026308
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-02-18 00:26:54 -0800

    Time: 1749 ms (5.02%), Space: 59.1 MB (5.69%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -9,66 +9,75 @@
  * };
  */
 class Solution {
-public:    
-    
-        ListNode* mergeTwoLists(ListNode* head1, ListNode* head2){
-         ListNode* head=new ListNode(-1);
-    ListNode* tail=head;
-    while(head1!=NULL && head2!=NULL){
-     if(head1->val<head2->val){
-         tail->next=head1;
-         tail=head1;
-         head1=head1->next;
-     }
-     else{
-         tail->next=head2;
-         tail=head2;
-         head2=head2->next;
-     }
-    }
-       if(head1!=NULL){
-         tail->next=head1;
-         tail=head1;
-     }
-     else{
-         tail->next=head2;
-         tail=head2;
-     }
-     return head->next;
-    }
-    
-       ListNode* middleNode(ListNode* head){
-            ListNode* first=head;
-            ListNode* second=head;
-          
-while(second->next!=NULL && second->next->next!=NULL){
-first=first->next;
-second=second->next->next;
-        
-        }
-    
-return first;
-    }
-    
-    ListNode* helper(ListNode* temp){
-          if(temp==NULL || temp->next==NULL){
-            return temp;
-        }
-        ListNode* Middle=middleNode(temp);
-         
-       ListNode* nxt=Middle->next;
-        Middle->next=NULL;
-        
-        ListNode* l1=helper(temp);
-       ListNode* l2= helper(nxt);
-       return mergeTwoLists(l1,l2);
-    }
-    
-   
-    ListNode* sortList(ListNode* head) {
+public:
+     ListNode* partition(ListNode* head, int x) {
+        ListNode* left=new ListNode;
+       ListNode* lefttail=left;
+        ListNode* right=new ListNode;
+       ListNode* righttail=right;
         ListNode* temp=head;
-     return helper(temp);
-        
+        int pivot=x;
+        while(temp!=NULL){
+            if(temp->val<pivot){
+                lefttail->next=temp;
+                lefttail=temp;
+                temp=temp->next;
+            }
+            else{
+                righttail->next=temp;
+                righttail=temp;
+                temp=temp->next;
+            }
+        }
+       lefttail->next=NULL;
+        righttail->next=NULL;
+        return left->next;
     }
     
+    void Randomize(ListNode* head){
+         int size=0;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            size++;
+            curr=curr->next;   
+        }
+        int randomno=rand()%size;
+        
+       curr=head;
+        while(randomno--){
+         curr=curr->next;   
+        }
+        swap(curr->val,head->val);
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL){
+            return NULL;
+        }
+        if(head->next==NULL){
+            return head;
+        }
+        
+        Randomize(head);
+       ListNode* partitionednode=partition(head,head->val);
+        ListNode* left=sortList(partitionednode);
+        ListNode* right=sortList(head->next);
+        
+        if(left==NULL){
+            head->next=right;
+            return head;
+        }
+            ListNode* lefttail=left;
+      while(lefttail->next!=NULL){
+          lefttail=lefttail->next;
+      }
+        if(right==NULL){
+             lefttail->next=head;
+            return left;
+        }
+    
+        head->next=right;
+        lefttail->next=head;
+        
+        return left;
+    }
 };
