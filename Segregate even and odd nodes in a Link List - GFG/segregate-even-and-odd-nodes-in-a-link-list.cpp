9dbf2a5188d53ae6a47b03c00f5a9b56368e1cf2
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-02-19 10:50:08 -0800

    Added solution - LeetHub

---
+++ Segregate even and odd nodes in a Link List - GFG/segregate-even-and-odd-nodes-in-a-link-list.cpp
@@ -0,0 +1,95 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+ Node* dummy=new Node(-1);
+ Node* tail=dummy;
+ Node* dummy2=new Node(-1);
+ Node* tail2=dummy2;
+ 
+ while(head!=NULL){
+     if(head->data%2==0){
+        tail->next=head;
+        tail=head;
+        head=head->next;
+     }
+     else{
+         tail2->next=head;
+         tail2=head;
+         head=head->next;
+     }
+ }
+ tail->next=dummy2->next;;
+ tail2->next=NULL;
+return dummy->next;
+ 
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+  // } Driver Code Ends
