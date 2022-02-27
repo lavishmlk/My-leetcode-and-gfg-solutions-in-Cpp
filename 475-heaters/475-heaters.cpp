5c63eb55464a71b9b48fb462acb7b2a05a9c13bc
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-02-27 09:52:00 -0800

    Time: 70 ms (77.62%), Space: 25.2 MB (98.19%) - LeetHub

---
+++ 475-heaters/475-heaters.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    
+   int bs(vector<int>& nums, int target){
+            int lo=0;
+        int hi=nums.size()-1;
+       //lowerbound me nums.size se hi initialize karte hai
+       int ans=nums.size();
+      while(lo<=hi){
+       
+            int mid=(lo+hi)/2;
+            
+            if(target<=nums[mid]){
+        ans=mid;
+             hi=mid-1;
+            }
+            else{
+                
+                lo=mid+1;
+            }
+        }
+        return ans; 
+ 
+        }
+        
+
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(heaters.begin(),heaters.end());
+        int Max=INT_MIN;
+      
+        for(int i=0;i<houses.size();i++){
+            int lbval=bs(heaters,houses[i]);
+            if(lbval==0){
+                int minval=abs(heaters[lbval]-houses[i]);
+          Max=max(Max,minval);
+                }
+            else if(lbval==heaters.size()){
+                  int ubval=lbval-1;
+                   int minval=abs(heaters[ubval]-houses[i]);
+                Max=max(Max,minval);
+            }
+            else if(abs(houses[i]-heaters[lbval])<abs(houses[i]-heaters[lbval-1])){
+                    int ubval=lbval-1;
+            int minval=abs(heaters[lbval]-houses[i]);
+               Max=max(Max,minval);
+            }
+                    else{
+              
+                    int ubval=lbval-1;
+            int minval=abs(heaters[ubval]-houses[i]);
+               Max=max(Max,minval);
+            }
+            
+        }
+        return Max;
+    }
+};
