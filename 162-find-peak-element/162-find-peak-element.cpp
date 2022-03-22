289aae368b7265f987ef9dd3d1cf31229ce61e53
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-03-22 03:16:04 -0700

    Time: 5 ms (51.03%), Space: 8.9 MB (50.83%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -15,10 +15,10 @@ public:
                 if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
                   return mid;
                 }
-                else if(nums[mid]<=nums[mid+1] && nums[mid]>nums[mid-1]){
+                else if(nums[mid]<nums[mid+1] && nums[mid]>nums[mid-1]){
                     low=mid+1;
                 }
-                else if(nums[mid]>nums[mid+1] && nums[mid]<=nums[mid-1]){
+                else if(nums[mid]>nums[mid+1] && nums[mid]<nums[mid-1]){
                     high=mid-1;
                 }
                 else{
