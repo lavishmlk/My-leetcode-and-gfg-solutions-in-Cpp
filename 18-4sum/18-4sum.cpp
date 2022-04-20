a505e934d0b6804a3ef9127995966cff01c835ca
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-04-20 05:20:32 -0700

    Time: 170 ms (30.40%), Space: 44.4 MB (20.81%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,60 +1,25 @@
 class Solution {
 public:
-    
-       vector<vector<int>> twopointer(vector<int>& nums,int left,int target){
-        vector<vector<int>>arr;
-           int right=nums.size()-1;
-           int start=left;
-            while(left!=right){
-                 if(left>start && nums[left]==nums[left-1]){
-                     left++;
-                        continue;
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+        sort(num.begin(), num.end());
+        unordered_map<int, set<pair<int, int>>> hash;
+        set<vector<int>> ans;
+        int n = num.size();
+        for (int i = 0; i < n; i ++) {
+            for (int j = i + 1; j < n; j ++) {
+                int a = num[i] + num[j];
+                if (hash.count(target - a)) {
+                    for (auto &p: hash[target - a]) {
+                        vector<int> b = {p.first, p.second, num[i], num[j]};
+                        ans.insert(b);
                     }
-                if(nums[left]+nums[right]==target){
-                    vector<int>arr1;
-                    arr1.push_back(nums[left]);
-                    arr1.push_back(nums[right]);
-                    arr.push_back(arr1);
-                    left++;
-                }
-                else if(nums[left]+nums[right]>target){
-                    right--;
-                }
-                else{
-                    left++;
                 }
             }
-       return arr;
-    }
-    
-  vector<vector<int>> ksum(vector<int>& nums, int target,int k,int start){
-     if(nums.size()<k){
-         vector<vector<int>>empty;
-         return empty;
-     }
-          vector<vector<int>>finalans;
-        if(k==2){
-           return twopointer(nums,start,target);
-        }
-                       
-                       
-        for(int i=start;i<=nums.size()-k;i++){
-            if(i>start && nums[i]==nums[i-1]){
-                continue;
-            }
-             vector<vector<int>>ans=ksum(nums, target-nums[i],k-1,i+1);
-            for(vector<int>pair:ans){
-                pair.push_back(nums[i]);
-                finalans.push_back(pair);
+            for (int j = 0; j < i; j ++) {
+                int a = num[j], b = num[i];
+                hash[a + b].insert(make_pair(a, b));
             }
         }
-    return finalans;     
-                       
-    }
-    
-    
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        return ksum(nums,target,4,0);
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
