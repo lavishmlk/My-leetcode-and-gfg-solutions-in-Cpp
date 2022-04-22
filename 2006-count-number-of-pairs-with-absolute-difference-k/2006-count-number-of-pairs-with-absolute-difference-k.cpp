506029cb1f348e189911bf83d457ad2d3e834139
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-04-22 14:06:15 -0700

    Time: 19 ms (68.86%), Space: 13.5 MB (18.44%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -9,9 +9,14 @@ public:
         }
         int Count=0;
         for(auto it:m){
+         
             if(m.find(it.first+k)!=m.end()){
+                  if(k==0)
+                Count=Count+it.second*(it.second-1)/2;
+                else
             Count=Count+it.second*m[it.first+k];
             }
+            
         }
         return Count;
     }
