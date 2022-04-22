5f34f9b79dca68788cf10ae157b35068a1369264
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-04-22 13:56:50 -0700

    Time: 15 ms (83.44%), Space: 13.5 MB (24.07%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    int pa(vector<int>& arr, int k){
-         int n=arr.size();
-          unordered_map<int,int>m;
-    int count=0;
-    
-    for(int i=0;i<n;i++){
-       auto it=m.find(k+arr[i]);
-       if(it!=m.end()){
-       int freq=it->second;
-           count=count+freq;
-       }
-  m[arr[i]]++;
+    int countKDifference(vector<int>& nums, int k) {
+        
+        unordered_map<int,int>m;
+        
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int Count=0;
+        for(auto it:m){
+            if(m.find(it.first+k)!=m.end()){
+            Count=Count+it.second*m[it.first+k];
+            }
+        }
+        return Count;
     }
-    
-    return count;
-    }
-    int countKDifference(vector<int>& arr, int k) {
-       int val1=pa(arr,k);
-        int val2=pa(arr,-k);
-        return val1+val2;
-    }
-    
-//         int getPairsCount(int arr[], int n, int k) {
-//     unordered_map<int,int>m;
-//     int count=0;
-    
-//     for(int i=0;i<n;i++){
-//        auto it=m.find(k-arr[i]);
-//        if(it!=m.end()){
-//        int freq=it->second;
-//            count=count+freq;
-//        }
-//   m[arr[i]]++;
-//     }
-    
-//     return count;
-//     }
 };
