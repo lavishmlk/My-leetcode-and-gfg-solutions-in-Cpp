c90e4a18951ed0f43d16a151fb79cc88ce82b529
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-05-31 14:05:26 -0700

    Time: 15 ms (65.69%), Space: 10.5 MB (95.44%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
+        // vector agar n*m hai to m*n ho jaega after transpose therefore m rows and n colums daal diye usme
         vector<vector<int>>ans(m,vector<int>(n));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
