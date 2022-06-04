4f21ecd8e13121ea88e967de14005c49264e46b0
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-06-04 02:46:10 -0700

    Time: 4 ms (88.77%), Space: 7.4 MB (49.05%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,67 +1,65 @@
 class Solution {
 public:
     
-    
-    vector<vector<string>>ans;
-    void  display( vector<vector<bool>>&grid){
-        string a="";
-    int n=grid.size();
-    vector<string>row;
-    
-    for (int i = 0; i < n; ++i)
-    {   
-        a="";
-        for (int j = 0; j < n; ++j){
-            
-                if(grid[i][j]==true){
-                    a=a+"Q";
-                }
-            else{
-                a=a+".";
+    vector<vector<string>> result;
+    bool issafe(vector<string>&board,int x,int y){
+       // for check in column
+       int n=board.size();
+       for(int row=0;row<x;row++){
+            if(board[row][y]=='Q'){
+                return false;
             }
-            
-            }
-        row.push_back(a);
-        }
-        ans.push_back(row);
-    }
-    
-  void  solve( vector<vector<bool>>&grid,int n,vector<bool>&cols,vector<bool>&diag1,vector<bool>&diag2,int row){
-          
-       if(row==grid.size()){
-           display(grid);
-           return;
        }
-       
-              for(int col=0;col<grid[0].size();col++){
-                  
-          
-                 if(cols[col]==false&&diag1[row+col]==false&&diag2[row-col+n]==false){
-                     
-                     diag1[row+col]=true;
-                     diag2[row-col+grid.size()]=true;
-                     cols[col]=true;
-                     
-                     grid[row][col]=true;
-                     solve(grid,n,cols,diag1,diag2,row+1);
-                     grid[row][col]=false;
-                     diag1[row+col]=false;
-                     diag2[row-col+grid.size()]=false;
-                     cols[col]=false;
-                 }
+
+       // for checking in left diagonal
+       int row=x-1,col=y-1;
+       while(row>=0 && col>=0){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            row--; col--;
+       }
+
+       // for checking in right diagonal
+        row=x-1;
+        col=y+1;
+       while(row>=0 && col<n){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            row--; col++;
+       }
+        //if all condition satisfied returning true;
+       return true;
+    }
+
+
+    void backtrack(int row , int n,vector<string>&curr) {
+        if(row == n){
+            result.push_back(curr);
+            return;
         }
         
-      }
-    
+        for(int col=0;col<n;col++) {
+            if(issafe(curr,row,col)) {
+                curr[row][col] = 'Q';
+                //recursive call to next row;
+                backtrack(row+1,n,curr);
+                //backtracting
+                //before backtracking we are change the location with previous value 
+                curr[row][col] = '.';
+            }
+        }
+        
+        return;
+    }
+     
     vector<vector<string>> solveNQueens(int n) {
-    vector<bool>diag1(2*n-1,false);
-        vector<bool>diag2(2*n-1,false);
-            vector<bool>col(n,false);
-        
-        vector<vector<bool>>grid(n,vector<bool>(n,false));
-        
-        solve(grid,n,col,diag1,diag2,0);
-        return ans;
-    
+        string s;
+        for(int i=0;i<n;i++) 
+            s.push_back('.');
+        vector<string> curr(n,s);
+        backtrack(0,n,curr);
+        return result;
     }
 };
