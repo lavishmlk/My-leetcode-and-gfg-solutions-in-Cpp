cc9687eafbe9c6cc57d5b61d7833a91d8294afca
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-06-04 12:27:49 -0700

    Time: 318 ms (53.16%), Space: 100.8 MB (49.12%) - LeetHub

---
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    //striver ka dekha
+    long long minimumRemoval(vector<int>& beans) {
+        
+        long long ans=1e18;
+         long long n=beans.size();
+         long long sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+beans[i];
+        }
+        sort(beans.begin(),beans.end());
+        
+        for( int i=0;i<n;i++){
+            long long change=sum-(n-i)*beans[i];
+ans=min(ans,change);
+        }
+        return ans;
+    }
+};
