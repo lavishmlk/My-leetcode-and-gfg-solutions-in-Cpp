9e67e3a75455fc9091f5fc19a51ff1ef4e87db10
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-06-06 13:30:56 -0700

    Time: 587 ms (85.04%), Space: 164 MB (20.28%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,22 +10,22 @@
 
 class Solution {
 public:
-    
+    //waise cloned tree pe normal find bhi laga sakte hai coz its given ki unique hai values see //previous submitted code
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
         
         if(original==NULL || cloned==NULL){
             return NULL;
         }
         
-        if(target->val==cloned->val){
+        if(original==target){
             return cloned;
         }
         
-     TreeNode* lclone= getTargetCopy(original,cloned->left,target);
+     TreeNode* lclone= getTargetCopy(original->left,cloned->left,target);
         if(lclone!=NULL){
             return lclone;
         }
-     TreeNode* rclone= getTargetCopy(original,cloned->right,target);
+     TreeNode* rclone= getTargetCopy(original->right,cloned->right,target);
         if(rclone!=NULL){
 return rclone;}
         
