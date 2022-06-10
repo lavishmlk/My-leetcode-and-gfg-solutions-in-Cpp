bd810eaa54479608b2d5875a8782369224f2f09b
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-06-10 06:27:47 -0700

    Time: 7 ms (28.74%), Space: 6.9 MB (99.38%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //from fraz video
+    
+   void helper(vector<int>& nums,vector<int>& ans,int idx,vector<vector<int>>& finalans){
+       
+       if(idx==nums.size()){
+           finalans.push_back(ans);
+           
+       }
+       else{
+       //yes call
+       ans.push_back(nums[idx]);
+       helper(nums,ans,idx+1,finalans);
+       //no call
+       ans.pop_back();
+       helper(nums,ans,idx+1,finalans);
+       }
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>finalans;
+        vector<int>ans;
+        
+        helper(nums,ans,0,finalans);
+        return finalans;
+        
+    }
+};
