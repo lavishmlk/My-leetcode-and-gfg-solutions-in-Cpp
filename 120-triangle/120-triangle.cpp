843959b9decf9681dc71acc031f5a0d8694d9cf6
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-06-13 03:13:47 -0700

    Time: 9 ms (49.75%), Space: 9 MB (26.05%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int helper(vector<vector<int>>& triangle, int i, int j, vector<vector<int>>& dp){
+      
+        if(i == triangle.size() ){
+            return 0 ;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j] ;
+        }   
+        
+        return dp[i][j] = triangle[i][j] + min(helper(triangle, i+1,j, dp), helper(triangle,i+1, j+1, dp)) ;
+        
+    }
+public:
+    int minimumTotal(vector<vector<int>>& triangle) { 
+        int n = triangle.size() ;
+        vector<vector<int>> dp(n, vector<int>(n, -1) ) ;
+        return helper(triangle, 0, 0, dp) ;
+    }
+};
