8bca53468432beb41315c4162815794cb4851c61
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-06-14 13:07:18 -0700

    Time: 72 ms (50.18%), Space: 41.9 MB (25.44%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,98 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        
+        if(root==NULL){
+            return "NULL,";
+        }
+        return to_string(root->val)+","+serialize(root->left)+serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data=="NULL,"){
+            return NULL;
+        }
+        
+        stack<pair<TreeNode*,char>>stk;
+        
+        char cstr[data.size() + 1];
+    strcpy(cstr, data.c_str()); 
+        
+        vector<string>v;
+//         char* ptr;
+//         ptr=strtok(cstr,",");
+    
+//         v.push_back(ptr);
+        
+//         while(ptr!=NULL){
+//             ptr=strtok(NULL,",");
+//             v.push_back(ptr);
+//         }
+        
+istringstream ss(data);
+string token;
+
+while(getline(ss, token, ',')) {
+    v.push_back(token);
+}
+        
+        TreeNode* root=NULL;
+        
+        for(string token:v){
+            if(token=="NULL"){
+                if(stk.top().second=='l'){
+                    pair<TreeNode*,char>s=stk.top();
+                    stk.pop();
+                    s.second='r';
+                    stk.push(s);
+                }
+                else
+                    stk.pop();
+            }
+            else{
+                TreeNode* n=new TreeNode(stoi(token));
+                if(stk.size()==0){
+                    root=n;
+                }
+                else{
+                       pair<TreeNode*,char>s=stk.top();
+                    if(stk.top().second=='l'){
+                      
+                        stk.pop();
+                        s.first->left=n;
+                        s.second='r';
+                        stk.push(s);
+                    }
+                    else{
+                        // s.pop();
+                        stk.pop();
+                        s.first->right=n;
+                        // stk.pop();
+                    }
+                    
+                }
+                stk.push({n,'l'});
+            }
+        }
+        return root;
+        
+        
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
