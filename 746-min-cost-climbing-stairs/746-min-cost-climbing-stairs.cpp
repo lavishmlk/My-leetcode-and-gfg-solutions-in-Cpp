3ff437c44d8579f6b62dbf4bb3509a2098212a01
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-07-10 23:55:00 +0530

    Time: 15 ms (27.88%), Space: 13.8 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    
-    int helper(vector<int>&cost,int n,vector<int>&dp){
+    //using tabulation, first see prev code using memo
+    int helper(vector<int>&cost,vector<int>&dp){
         
-        //ye bhi ar sakte the but here we have to handle seperately for size of array less than 4 coz these dp 1 will not be filled ans will always come -1
-        // if(n==cost.size()-1 || n==cost.size()-2){
-        //     return cost[n];
-        // }
-        if(n>=cost.size()){
-            return 0;
+        int n=cost.size();
+        for(int i=n-1;i>=0;i--){
+       
+          int  val1=(i+1<=cost.size()-1)?dp[i+1]:0;
+        int val2=(i+2<=cost.size()-1)?dp[i+2]:0;
+          dp[i]= min(val1,val2)+cost[i];
+         
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int val1=helper(cost,n+1,dp);
-        int val2=helper(cost,n+2,dp);
         
-        return dp[n]=cost[n]+min(val1,val2);
+        return min(dp[0],dp[1]);
     }
     
     int minCostClimbingStairs(vector<int>& cost) {
        vector<int>dp(cost.size()+1,-1);
-       
-       helper(cost,0,dp);
+     return helper(cost,dp);
     
-       return min(dp[0],dp[1]);
     }
 };
