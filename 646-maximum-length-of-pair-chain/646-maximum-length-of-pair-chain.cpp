b7ec3edc0ec801b2337a7201d0ac9c563fb7f11d
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-07-12 03:15:00 +0530

    Time: 75 ms (91.71%), Space: 22.5 MB (72.93%) - LeetHub

---
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   //same like n meetings in 1 room(gfg) or non overlapping intervals 
+        static bool comp( vector<int> &a, vector<int> &b){
+         if(a[1]==b[1]){
+           return  a[0]<b[0];
+         }
+       return  a[1]<b[1];
+     }
+    
+    int findLongestChain(vector<vector<int>>& intervals) {
+             int n=intervals.size();
+            sort(intervals.begin(),intervals.end(),comp);
+      
+      if(intervals.size()==0){
+          return 0;
+      }
+
+    int count=0;
+
+int limit=INT_MIN;
+      for(int i=0;i<n;i++){
+          if(limit<intervals[i][0]){
+              count++;
+              limit=intervals[i][1];
+          }
+          
+      }
+      return count;
+    }
+};
