5f66b390c31aa7a3d598890e208e95b81462f42c
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-07-25 20:22:13 +0530

    Time: 75 ms (89.99%), Space: 11 MB (78.47%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
+    //archit lec 9
+    //make all calls and reduce amount and base cASE IS WHEN AMOUNT BECOMES 0
+    
+   int  memo(vector<int>&coins,int amount,int  dp[]){
+        
+       if(amount==0){
+           return 0;
+       }
+       if(dp[amount]!=-1){
+           return dp[amount];
+       }
+       
+        int Min=INT_MAX;
+       for(int i=coins.size()-1;i>=0;i--){
+           if(amount-coins[i]<0){
+               continue;
+           }
+              int val=memo(coins,amount-coins[i],dp);  
+         Min=min(val,Min);
+       }   
+  
+       //this is necessary to prevent int overflow
+       if(Min<INT_MAX)
+           Min=Min+1;
+       return dp[amount]=Min;
+    }
+    
+    
     int coinChange(vector<int>& coins, int amount) {
-        
-   int coinsize=coins.size();
-        
-        int dp[amount+1];
-      
-        for(int i=1;i<=amount;i++){
-       dp[i]=amount+1;
+         int dp[amount+1];
+        for(int i=0;i<amount+1;i++){
+            dp[i]=-1;
         }
-          dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<coinsize;j++){
-                if(coins[j]<=i){
-                    dp[i]=min(dp[i],dp[i-coins[j]]+1);
-                }
-            }
+    int val=memo(coins,amount,dp);
+        if(val!=INT_MAX){
+            return val;
         }
-          return dp[amount] > amount ? -1 : dp[amount];
+        return -1;
     }
 };
