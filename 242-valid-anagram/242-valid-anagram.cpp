c4770ef4e7cafb18b4fcb07b84f6f8a77bb5074e
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-07-28 23:39:21 +0530

    Time: 45 ms (5.61%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -16,12 +16,27 @@ public:
             else
                 return false;
         }
-       for(int i=0;i<m.size();i++){
-           if(m[i]>0){
-               return false;
-           }
-           
-       }
+    for(auto it=m.begin();it!=m.end();it++){
+        if(it->second>0){
+            return false;
+        }
+    }
         return true;
     }
 };
+//m2
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()) return false;
+//         int n = s.length();
+//         int counts[26] = {0};
+//         for (int i = 0; i < n; i++) { 
+//             counts[s[i] - 'a']++;
+//             counts[t[i] - 'a']--;
+//         }
+//         for (int i = 0; i < 26; i++)
+//             if (counts[i]) return false;
+//         return true;
+//     }
+// };
