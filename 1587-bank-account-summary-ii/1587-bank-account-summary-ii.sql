cb82951944d797220d38e61306cbb58778419579
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-07-30 17:37:49 +0530

    Time: 1506 ms (13.94%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 # //having is always used after group by
 select Users.name, sum(amount) as balance
+# or can also write like this-> select Users.name, sum(amount)  balance
 from  Transactions join Users USING (account)
 GROUP BY account
 having balance>10000
