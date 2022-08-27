2e703a82dd00cb91c74ce387710929843dc58587
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-08-27 15:23:34 +0530

    Time: 496 ms (9.71%), Space: 84.2 MB (92.13%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,28 +1,26 @@
 class StockSpanner {
-       stack<pair<int,int>>st;
-        int index;
 public:
+    //just find next greter on left and subtract from current index to get consecutive days
+    stack<pair<int,int>>st;
+     int index;
     StockSpanner() {
-     
-          index=-1;
-        
+     index=-1;
     }
     
     int next(int price) {
-      index+=1;
-        
+        index+=1;
         while(st.size()>0 && st.top().first<=price){
             st.pop();
         }
-        
         if(st.size()==0){
             st.push({price,index});
             return index+1;
         }
+        
+        
         int result=st.top().second;
         st.push({price,index});
         return index-result;
-        
     }
 };
 
