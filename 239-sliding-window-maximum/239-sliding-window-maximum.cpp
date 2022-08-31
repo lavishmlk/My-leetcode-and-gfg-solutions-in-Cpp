bba04ff0f22b6b70b6e77a0fef1e499a9422e2b1
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-08-31 12:48:43 +0530

    Time: 295 ms (97.17%), Space: 134.6 MB (92.33%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,81 +1,37 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        int window=k;
+            vector<int>ans;
         deque<int>q;
-        vector<int>ans;
         
-        // int Max=INT_MIN;
-        
-        for(int i=0;i<window;i++){
-            if(q.size()==0){
+        for(int i=0;i<k;i++){
+              if(q.size()==0){
                 q.push_back(nums[i]);
                 continue;
             }
-            // Max=max(nums[i],Max);
-            if(nums[i]>q.back()){
-            while(q.size()>0 && nums[i]>q.back()){
+            while(q.size()>0 && q.back()<nums[i]){
                 q.pop_back();
             }
             q.push_back(nums[i]);
-            }
-            else{
-                q.push_back(nums[i]);
-            }
         }
-        
         ans.push_back(q.front());
-        
-        // q.push_back(Max);
-        
-        for(int i=window;i<nums.size();i++){
-            
-            if(nums[i-k]==q.front()){
+        for(int i=k;i<nums.size();i++){
+            if(q.front()==nums[i-k]){
                 q.pop_front();
             }
-            
-             if(nums[i]>q.back()){
-            while(q.size()>0 && nums[i]>q.back()){
+               while(q.size()>0 && q.back()<nums[i]){
                 q.pop_back();
             }
-            q.push_back(nums[i]);
+             q.push_back(nums[i]);
+            
+            if(nums[i]>q.front()){
+                ans.push_back(nums[i]);
             }
             else{
-                q.push_back(nums[i]);
+                ans.push_back(q.front());
             }
-            
-          ans.push_back(q.front());
-
-            
-            
-            
-//             if(q.size()==0){
-//                int Max2=INT_MIN;
-//                   int x=i;
-//                 int y=window;
-//                 while(y-->0){
-               
-//                      Max2=max(nums[x],Max2);
-//                      x--;
-//                 }
-//                   q.push_back(Max2);
-//                 ans.push_back(Max2);
-               
-//             }
-            
-//             else{
-//             if(nums[i]>q.front()){
-//                 q.pop_front();
-//                 q.push_back(nums[i]);
-//                 ans.push_back(nums[i]);
-//             }
-//             else{
-//                 ans.push_back(q.front());
-//             }
-//             }
+           
         }
         return ans;
-        
     }
 };
