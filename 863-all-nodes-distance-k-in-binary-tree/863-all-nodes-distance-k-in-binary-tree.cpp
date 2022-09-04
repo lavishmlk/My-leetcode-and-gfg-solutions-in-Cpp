ca0d4d8d8b8fd6780f5587c378dcc6da3f4df0e3
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-09-04 22:09:18 +0530

    Time: 15 ms (27.79%), Space: 12.5 MB (86.80%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,90 +7,70 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//done by me 2 aur archit ke tarike wle codes bhi hai wo bhi dekhlio
+//node to root path mangwao aur k levels down fir print kardo
 class Solution {
 public:
-    //THIS IS METHOD 2 BY ARCHIT TO AVOID EXTRA SPACE BUT METHOD 1 IS EASY JO PEHLE SUBMIT KIYA HUA HAI //WITH EXTRA SPACE
-int DFS(TreeNode* root, TreeNode* target, int k, vector<int>& res){
-        if(root == NULL) return -1;
-        if(root == target){
-            Kdistance(root, NULL, k, res);
-            return 1;
-        }
-        
-        int left = DFS(root->left, target, k, res);
-        if(left >= 0){
-            Kdistance(root, root->left, k - left, res);
-            return left + 1;
-        }
-        
-        int right = DFS(root->right, target, k, res);
-        if(right >= 0){
-            Kdistance(root, root->right, k - right, res);
-            return right + 1;
-        }
-        
-      return -1;  }
-    
-//     vector<TreeNode*>nodetorootpath(TreeNode* n,int val){
-    
-//     vector<TreeNode*>v;
-//     if(n==NULL){
-//         return v;
-//     }
-//     if(n->val==val){
-//           v.push_back(n);
-//           return v;
-//     }
-    
-//    vector<TreeNode*>v1=nodetorootpath(n->left,val);
-//    if(v1.size()>0){
-     
-//               v1.push_back(n);
-       
-//        return v1;
-    
-//    }
-//    vector<TreeNode*>v2=nodetorootpath(n->right,val);
-//    if(v2.size()>0){
-       
-//            v2.push_back(n);
-       
-//        return v2;
-//    }
-   
 
-//    return v;
-   
-// }
-
-
-void Kdistance(TreeNode* root, TreeNode* blocker, int k, vector<int>& res){
-        if(k < 0 || root == NULL || root == blocker)  return;
-        if(k == 0){
-            res.push_back(root->val);
-            return;
-        }
-        
-        Kdistance(root->left, blocker, k - 1, res);
-        Kdistance(root->right, blocker, k - 1, res);
+    vector<TreeNode*>nodetorootpath(TreeNode* n,int val){
+    
+    vector<TreeNode*>v;
+    if(n==NULL){
+        return v;
     }
+    if(n->val==val){
+          v.push_back(n);
+          return v;
+    }
+    
+   vector<TreeNode*>v1=nodetorootpath(n->left,val);
+   if(v1.size()>0){
+     
+              v1.push_back(n);
+       
+       return v1;
+    
+   }
+   vector<TreeNode*>v2=nodetorootpath(n->right,val);
+   if(v2.size()>0){
+       
+           v2.push_back(n);
+       
+       return v2;
+   }
+   
 
+   return v;
+   
+}
 
-    vector<int> KDistanceNodes(TreeNode* root, TreeNode* target , int k)
+void nodeskleveldown(TreeNode *root, int k,vector<int>&ans,TreeNode* blocker){
+      if(root==NULL || k<0 || root==blocker){
+     
+      return ;
+  }
+  if(k==0){
+      ans.push_back(root->val);
+  }
+  nodeskleveldown(root->left,k-1,ans,blocker);
+  nodeskleveldown(root->right,k-1,ans,blocker);
+}
+vector<int>ans;
+    vector <int> KDistanceNodes(TreeNode* root, int target , int k)
     {
         
-//        vector<TreeNode*>path=nodetorootpath(root,target);
-//         for(int i=0;i<path.size() && i<=k;i++){  
-//               vector<int>v=Kdistance(path[i],k-i,i==0?NULL:path[i-1]);
-//               if(v.size()>0){
-//                    for(int i=0;i<v.size();i++){
-//                   ans.push_back(v[i]);
-//               }
-//               }
+       vector<TreeNode*>path=nodetorootpath(root,target);
+        for(int i=0;i<path.size() && i<=k;i++){
+            vector<int>v;
+              nodeskleveldown(path[i],k-i,v,i==0?NULL:path[i-1]);
+              if(v.size()>0){
+                   for(int i=0;i<v.size();i++){
+                  ans.push_back(v[i]);
+              }
+              }
              
-//  }
-          vector<int>ans;
-        DFS(root,target,k,ans);
+        }
+            // sort( ans.begin(), ans.end() );
       return ans;
     }
     
@@ -98,8 +78,8 @@ void Kdistance(TreeNode* root, TreeNode* blocker, int k, vector<int>& res){
     
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         
-        
-      return KDistanceNodes(root,target,k);
+        int x=target->val;
+      return KDistanceNodes(root,x,k);
         
     }
 };
