90b4ec23819f548721c1da18c9ed4e58cdab6326
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-09-09 11:59:03 +0530

    Time: 587 ms (87.06%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 # //having is always used after group by
-select Users.name, sum(amount) as balance
+select name, sum(amount) as balance
 # or can also write like this-> select Users.name, sum(amount)  balance
 from  Transactions join Users USING (account)
 GROUP BY account
