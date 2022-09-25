f817734c9c8aee8646b70af36d506b5440406cdc
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-09-25 23:01:27 +0530

    Time: 14 ms (48.31%), Space: 13.1 MB (29.96%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     //dp video 13 l1
-    //greedy wla easy tha last soln
+    //greedy wla easy tha last to last soln
+    //first see previous code without space optimization
     //buy ke baad sell hi karna hoga aur sell ke baad buy
     //logic-using dp table: if buying is necessary to sell kare hue profit me buy wla subtract karlo ya fir buy na karo tab pehle se jo profit aa rha hai dono me se jo bhi max hai
     //similar logic for sell
     int maxProfit(vector<int>& prices) {
         
-        int *buy=new int[prices.size()];
-        int *sell=new int[prices.size()];
-        
-        buy[0]=-prices[0];
-        sell[0]=0;
+        int buy=-prices[0];
+        int sell=0;
         
         for(int i=1;i<prices.size();i++){
             
-            buy[i]=max(sell[i-1]-prices[i],buy[i-1]);
-            sell[i]=max(buy[i-1]+prices[i],sell[i-1]);
+           int newbuy=max(sell-prices[i],buy);
+            int newsell=max(buy+prices[i],sell);
+            
+            buy=newbuy;
+            sell=newsell;
         }
         
-        return sell[prices.size()-1];
+        return sell;
     }
 };
