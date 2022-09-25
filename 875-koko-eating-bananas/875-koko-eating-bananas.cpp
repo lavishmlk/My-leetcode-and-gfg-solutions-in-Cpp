4b33026d48cf1388863743ea8a0db860f0a4daf4
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-09-25 13:00:26 +0530

    Time: 115 ms (23.10%), Space: 35 MB (5.33%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    
-bool isPossible(vector<int>&piles,int mid,int h){
+    //tc-m*log(max)
+bool isPossible(vector<int>piles,int mid,int h){
      
-    int myhrs=0;
+    long myhrs=0;
     for(int i=0;i<piles.size();i++){
+        //it will give floor
      myhrs=myhrs+piles[i]/mid;
+        //to get ceil we have to do this if not fully dividible
         if(piles[i]%mid!=0){
             myhrs++;
         }
     }
+
     if(myhrs<=h){
         return true;
     }
@@ -19,8 +22,6 @@ bool isPossible(vector<int>&piles,int mid,int h){
     }
     
     int minEatingSpeed(vector<int>& arr, int h) {
-          // int M=h;
-        // int N=arr.size();
   
        int Max=INT_MIN;
         for(int i=0;i<arr.size();i++){
@@ -44,3 +45,16 @@ bool isPossible(vector<int>&piles,int mid,int h){
         return ans;
     }
 };
+
+    //i did this but its giving tle
+   // int i=0;
+//     while(i<piles.size()){
+        
+//         if(piles[i]-mid<=0){
+//             piles[i]=piles[i]-mid;
+//             i++;
+//         }
+//         else{
+//         piles[i]=piles[i]-mid;}
+//         myhrs++;
+//     }
