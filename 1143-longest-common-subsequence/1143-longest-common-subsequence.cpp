b215230507768c429d16815edc55f06978f47183
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-07 02:16:49 +0530

    Time: 57 ms (25.59%), Space: 13.1 MB (46.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
+    
+    //tabulation-using aditya verma logic ,tc & sc->n*m i.e size of str1*size of str2
+  int longestCommonSubsequence(string s1, string s2) {
+int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+    }
+};
+
+// class Solution {
+// public:
     //Striver takeyouforward article
     //recursive
     //O(N*M)
     //Space Complexity: O(N*M) + O(N+M), array+recusrsive
-    int lcsUtil(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
+//     int lcsUtil(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
 
-    if(ind1<0 || ind2<0)
-        return 0;
+//     if(ind1<0 || ind2<0)
+//         return 0;
         
-    if(dp[ind1][ind2]!=-1)
-        return dp[ind1][ind2];
+//     if(dp[ind1][ind2]!=-1)
+//         return dp[ind1][ind2];
     
-    if(s1[ind1] == s2[ind2])
-        return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
+//     if(s1[ind1] == s2[ind2])
+//         return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
     
-    else 
-        return dp[ind1][ind2] = 0 + max(lcsUtil(s1,s2,ind1,ind2-1,dp),lcsUtil
-       (s1,s2,ind1-1,ind2,dp));
+//     else 
+//         return dp[ind1][ind2] = 0 + max(lcsUtil(s1,s2,ind1,ind2-1,dp),lcsUtil
+//        (s1,s2,ind1-1,ind2,dp));
    
-}
+// }
 
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-    int m=text2.size();
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n=text1.size();
+//     int m=text2.size();
 
-    vector<vector<int>> dp(n,vector<int>(m,-1));
-    return lcsUtil(text1,text2,n-1,m-1,dp);
-    }
-};
+//     vector<vector<int>> dp(n,vector<int>(m,-1));
+//     return lcsUtil(text1,text2,n-1,m-1,dp);
+//     }
+// };
