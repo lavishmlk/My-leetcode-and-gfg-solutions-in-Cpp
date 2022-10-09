3bd860c730b9fc29147e5f89380d1e2487b39be9
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-09 20:05:57 +0530

    Time: 394 ms (29.85%), Space: 69.6 MB (66.19%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,21 @@
+//(x%k + k)%k is done to counter negative integers in the array-see fraz video after 10
+
+//solution by pep-rem and k-rem ki freq same honi chahiye aur 0 remainder ki freq even honi chahiye
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        if(arr.size()&1) return false;
+        unordered_map<int,int>m;
+        for(auto x:arr) m[(x%k + k)%k]++;       //store the count of remainders in a map.
+           for(int i=0;i<arr.size();i++)
+        {
+            int rem=(arr[i]%k + k)%k;
+            if(rem==0)                         //if the remainder for an element is 0 then the count of numbers that give this remainder must be even.
+            { 
+                if(m[rem]%2!=0) return false;   //if count of numbers that give this remainder is odd all pairs can't be made hence return false.
+            }         
+            else if(m[rem] != m[k - rem]) return false;    //if the remainder rem and k-rem do not have the same count then pairs can not be made 
+        }
+        return true;
+    }
+};
