ce84a0e706e123f96871b5bbceeda858130cdd6d
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-11 19:14:44 +0530

    Time: 32 ms (13.52%), Space: 7.5 MB (13.35%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        unordered_map<int,int>m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            if(m.find(t[i])!=m.end()){
+                if(m[t[i]]>0)
+                m[t[i]]--;
+                else
+                    m.erase(t[i]);
+            }
+            else
+                return false;
+        }
+    for(auto it=m.begin();it!=m.end();it++){
+        if(it->second>0){
+            return false;
+        }
+    }
+        return true;
+    }
+};
+//m2
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()) return false;
+//         int n = s.length();
+//         int counts[26] = {0};
+//         for (int i = 0; i < n; i++) { 
+//             counts[s[i] - 'a']++;
+//             counts[t[i] - 'a']--;
+//         }
+//         for (int i = 0; i < 26; i++)
+//             if (counts[i]) return false;
+//         return true;
+//     }
+// };
