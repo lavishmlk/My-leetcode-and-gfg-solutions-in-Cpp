f007efa63ace743e9b89457117fb75cfa42395b6
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-19 02:31:16 +0530

    Time: 71 ms (34.08%), Space: 11.5 MB (25.46%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,85 @@
+//M1 stack discussion
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+class Solution {
+public:
+    
+    string removeDuplicates(string str) 
+    {
+        stack<char> s;
+        for(auto i:str)
+        {
+            if(s.empty())
+                s.push(i);
+            else
+            {
+                if(s.top()==i)
+                {
+                    s.pop();
+                }
+                else
+                    s.push(i);
+            }
+        }
+        string ans;
+        while(!s.empty())
+            ans+=s.top(),s.pop();
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//m2
+// Solution 2:(Using String as Stack - Accepted)
+// It is just the simple modification of above code,
+// instead of using stack we can directly use the resultant string for performing pop and push operations.
+// class Solution {
+// public:
+    
+//     string removeDuplicates(string str) 
+//     {
+//         string ans;
+//         for(auto i:str)
+//         {
+//             if(!ans.empty() and ans.back()==i)
+//                 ans.pop_back();
+//             else
+//                 ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
+
+
+//m3 recursion-memory limit ecxceeded
+// class Solution {
+// public:
+//     //discuss
+//     //recursive tle
+//     //agar equal nahi hai tabhji index bandhana hai else nahi badha taaki 1st test case me jab bb remove hoke aa ban jae tab aa bhi remove ho jae
+//     string ans;
+//     void go(string str,int idx)
+//     {
+//         if(idx>=str.length())
+//         {
+//             ans=str;
+//             return;
+//         }
+//         if(str[idx]==str[idx+1])
+//         {
+//             if(idx==0)
+//                 go(str.substr(idx+2),0);
+//             else
+//                 go(str.substr(0,idx)+str.substr(idx+2),0);
+//         }
+//         else
+//         go(str,idx+1);
+//     }
+//     string removeDuplicates(string s) 
+//     {
+//         ans="";
+//         go(s,0);
+//         return ans;
+//     }
+// };
