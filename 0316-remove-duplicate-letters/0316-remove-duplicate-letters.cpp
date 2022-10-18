fb00939ea3ce39c49f458fa231e45a2a2a391e19
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-18 17:15:09 +0530

    Time: 8 ms (38.22%), Space: 6.9 MB (30.80%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         
-        // int* freq=new int[26];
+        //youtube pepcoding
+        //using stack->logic is: hume mainly stack me push karne hai elements agar wo st.top wle element se bada hai aur abhi tak nahi aya to(ye check karne ke liye exist array banaya hai)
+        //else agar wo pehle aa chuka hai tab nahi aega doobara aur ya fir agar st.top ka element present element jo push karna hai usse bada hai to check ki st.top wla element aage future me aega(ye check karne ke liye freq array banaya hai)? agar aaega to baad me push kar denge abhi st.pop karde 
        vector<int>freq(26,0);
        vector<bool>exists(26,false);   
         stack<char>st;
@@ -17,7 +19,6 @@ public:
         
            while(st.size()>0 && freq[st.top()-'a']>0 && st.top()>s[i]){
                 exists[st.top()-'a']=false;
-                // freq[st.top()]++;
                 st.pop();
             }
             st.push(s[i]);
