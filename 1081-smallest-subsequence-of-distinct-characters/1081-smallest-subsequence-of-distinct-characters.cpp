64ddca05c375494bb5b7d0e31e18ec1410339fba
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-18 17:42:53 +0530

    Time: 5 ms (36.58%), Space: 6.5 MB (39.87%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        //Same like leetcode 316
+            //using stack->logic is: hume mainly stack me push karne hai elements agar wo st.top wle element se bada hai aur abhi tak nahi aya to(ye check karne ke liye exist array banaya hai)
+        //else agar wo pehle aa chuka hai tab nahi aega doobara aur ya fir agar st.top ka element present element jo push karna hai usse bada hai to check ki st.top wla element aage future me aega(ye check karne ke liye freq array banaya hai)? agar aaega to baad me push kar denge abhi st.pop karde 
+       vector<int>freq(26,0);
+       vector<bool>exists(26,false);   
+        stack<char>st;
+        for(int i=0;i<s.length();i++){   
+        freq[s[i]-'a']++;
+        }
+        
+        for(int i=0;i<s.length();i++){
+             freq[s[i]-'a']--;
+            if(exists[s[i]-'a']==true)
+                continue;
+        
+           while(st.size()>0 && freq[st.top()-'a']>0 && st.top()>s[i]){
+                exists[st.top()-'a']=false;
+                st.pop();
+            }
+            st.push(s[i]);
+            exists[s[i]-'a']=true;
+        }
+        string ans="";
+        while(st.size()>0){
+        ans=ans+st.top();
+        st.pop();}
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
