ffb47fb747ac3e85a30af98f7c90c1f8ba5741ae
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-20 22:57:43 +0530

    Time: 99 ms (22.90%), Space: 19.3 MB (5.06%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -8,20 +8,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//recursive approach thoda bahot khud kia tha thoda discuss se lia
+//
 class Solution {
 public:
-    //   private ListNode temp;
-    // private boolean isStop;
 
      void reorder(ListNode* &head,bool &isStop,ListNode* &temp){
         if (head == NULL) return;
         reorder(head->next,isStop,temp);
 
         if (!isStop) {
-            ListNode* next = temp->next;
-            temp->next = head;
-            head->next = next;
-            temp = next;
+                head->next=temp->next;
+        temp->next=head;
+        temp=head->next;
         }
 
         if (temp != NULL && temp->next == head) {
@@ -36,3 +36,75 @@ public:
         reorder(head,isStop,temp);
     }
 };
+
+//by archit_>find middle then reverse from middle then use merge sort type of approach
+// class Solution {
+// public:
+    
+//       ListNode* reverseList(ListNode* head) {
+ 
+//      ListNode* prev=NULL;
+//         ListNode* curr=head;
+            
+//         while(curr!=NULL){
+//             ListNode* ahead=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=ahead;
+//         }
+//         head=prev;
+//         return head;
+        
+//     }
+    
+//       ListNode* middleNode(ListNode* head) {
+//       ListNode* first=head;
+//         ListNode* second=head;
+        
+//         while(second!=NULL && second->next!=NULL){
+// first=first->next;
+// second=second->next->next;}
+// return first;
+//     }
+    
+//     void reorderList(ListNode* head) {
+//         if(head==NULL || head->next==NULL){
+//             return;
+//         }
+//         ListNode* middle=middleNode(head);
+//      ListNode* reversednode=reverseList(middle);
+//         ListNode* curr=head;
+//         ListNode* prev=NULL;
+//         while(curr!=middle){
+//             prev=curr;
+//             curr=curr->next;
+//         }
+//         prev->next=NULL;
+        
+//         ListNode*head1=head;
+//         ListNode* head2=reversednode;
+//          ListNode* dummy=new ListNode(-1);
+//        ListNode* tail=dummy;
+//         // int Count=0;
+//         while(head1!=NULL || head2!=NULL){   
+//                  // if(Count%2==0 && head1!=NULL){ ye line likhega tab bhi chal jaega waise par no need
+//             if(Count%2==0 && head1!=NULL){
+              
+//             tail->next=head1;
+                  
+//             tail=head1; 
+//                  head1=head1->next;
+//                 // Count++;
+//             }
+//             else if(head2!=NULL){
+                  
+//                 tail->next=head2;
+
+//                 tail=head2;
+//                 head2=head2->next;
+//                 // Count++;
+//             }
+//         }
+//         head=dummy->next;
+//     }
+// };
