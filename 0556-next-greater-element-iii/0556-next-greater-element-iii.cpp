9fb2c9232b1f3ebb7de75a62c0ed2094212e4727
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-21 22:57:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (66.00%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,92 @@
+
+
+class Solution {
+public:
+    int nextGreaterElement(int num) {
+          string s = to_string(num);
+        int i = s.length() - 2;
+        while(i >= 0 && s[i] >= s[i + 1]) {
+            i--;
+        }
+        
+        if(i >= 0) {
+            int k = s.length() - 1;
+            while(s[k] <= s[i]) {
+                k--;
+            }
+            char temp = s[i];
+            s[i] = s[k];
+            s[k] = temp;
+        } else {
+            return -1;
+        }
+        
+        string res = "";
+        for(int j = 0; j <= i; j++) {
+            res += s[j];
+        }
+        
+        for(int j = s.length() - 1; j > i; j--) {
+            res += s[j];
+        }
+        
+      if(stol(res)>INT_MAX){
+          return -1;
+      }
+        else{
+            return stol(res);
+        }
+//         if(num < 12)return -1;
+        
+//         string s = to_string(num);
+        
+//         int n = s.size();
+        
+//         int i = n-2;
+        
+//         while(i>=0 && s[i]>=s[i+1])
+//             i--;
+        
+        
+//         if(i<0)
+//             return -1;
+        
+//         int j = n-1;
+        
+//         int k;
+        
+//         char max = '-';
+        
+//         while(i<j){
+            
+//             if(s[i]<s[j]){
+                
+//                 if(max == '-'){
+//                     k = j;
+//                     max = s[j];
+//                 }else if(s[j]<max){
+//                     k = j;
+//                     max = s[j];
+//                 }
+                
+//             }
+            
+//             j--;
+//         }
+//         swap(s[i],s[k]);
+        
+//         i = j+1;
+//         j = n-1;
+        
+//         while(i<j){
+//             swap(s[i],s[j]);
+//             i++;j--;
+//         }
+        
+//         long ans = stol(s);
+
+//         if(ans>INT_MAX)return -1;
+        
+//         return ans;
+    }
+};
