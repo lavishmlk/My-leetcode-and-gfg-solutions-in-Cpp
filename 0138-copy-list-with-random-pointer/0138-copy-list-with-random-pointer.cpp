b3d045a1782c674c95c8889b7e93a5b1b3fb9a98
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-22 23:33:16 +0530

    Time: 23 ms (13.78%), Space: 11.3 MB (27.08%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -13,6 +13,8 @@ public:
     }
 };
 */
+//archit last video 
+//with extra space of map O(n)
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
@@ -34,6 +36,15 @@ public:
         Node* x=dummy->next;
         while(temp2!=NULL){
 
+            //agar auto it use karna hai to use this syntax coz temp2->random can be null
+//                 if(temp2->random){
+//                     auto it=m.find(temp2->random);
+//             x->random=it->second;
+//             }
+// else{
+//     x->random=NULL;
+// }
+            
             x->random=m[temp2->random];
             temp2=temp2->next;
             x=x->next;
