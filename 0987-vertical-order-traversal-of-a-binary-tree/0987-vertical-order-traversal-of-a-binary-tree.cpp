df4d429d967a9cb06f7e43ec159d52487ec25145
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-26 03:01:56 +0530

    Time: 12 ms (32.84%), Space: 14.9 MB (11.22%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Another solution using 2d map -it was in archits vides isse pichla submission is from evernote
+    map<int,map<int,vector<int>>>m;
+    void DFS(TreeNode* root,int col,int row){
+        
+        if(root==NULL){
+            return;
+        }
+        
+        m[col][row].push_back(root->val);
+        
+        DFS(root->left,col-1,row+1);
+        DFS(root->right,col+1,row+1);
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root){
+          vector<vector<int>>arr;
+        if(root==NULL){
+          
+            return arr; 
+        }
+        
+        DFS(root,0,0);
+        
+        
+        //for col
+        for(auto i:m){
+            vector<int>oned;
+            //for row
+            for(auto j:i.second){
+                vector<int>a;
+                for(auto k:j.second){
+                    a.push_back(k);
+                 
+                }
+                   sort(a.begin(),a.end());
+                 
+                while(a.size()>0){
+                    int p=a.front();
+                 a.erase(a.begin());
+                    oned.push_back(p);
+                }
+               
+            }
+           
+            arr.push_back(oned);
+        }
+        return arr;
+    }
+};
