0416e9f93393838a460b7d8f643b4e0457f1c5d6
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-25 12:00:27 +0530

    Time: 8 ms (59.56%), Space: 11.2 MB (97.08%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,23 +1,66 @@
+
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int i = 0, j = 0; // word pointers
+        int m = 0, n = 0; // char pointers
         
-        string str1,str2;
-        
-        for(int i=0;i<word1.size();i++){
-            for(char c:word1[i]){
-               str1+=c;
-            }
+        while (i < word1.size() and j < word2.size()) {            
+            if (word1[i][m++] != word2[j][n++])
+                return false;
+            
+            if (m >= word1[i].size())
+                i++, m = 0;
+            
+            if (n >= word2[j].size())
+                j++, n = 0;
         }
         
-            for(int i=0;i<word2.size();i++){
-            for(char c:word2[i]){
-               str2+=c;
-            }
-        }         
-  if(str1==str2){
-      return true;
-  } 
-        return false;
+        return i == word1.size() and j == word2.size();
     }
 };
+
+
+//m2
+// class Solution {
+// public:
+//     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        
+//         string str1, str2;
+        
+//         for( auto w1 : word1 ){
+//             str1 += w1;
+//         }
+        
+//         for( auto w2 : word2 ){
+//             str2 += w2;
+//         }
+        
+//         return str1 == str2;
+//     }
+// };
+
+//by me
+// class Solution {
+// public:
+//     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        
+//         string str1,str2;
+        
+//         for(int i=0;i<word1.size();i++){
+//             for(char c:word1[i]){
+//                str1+=c;
+//             }
+//         }
+        
+//             for(int i=0;i<word2.size();i++){
+//             for(char c:word2[i]){
+//                str2+=c;
+//             }
+//         }         
+//   if(str1==str2){
+//       return true;
+//   } 
+//         return false;
+//     }
+// };
