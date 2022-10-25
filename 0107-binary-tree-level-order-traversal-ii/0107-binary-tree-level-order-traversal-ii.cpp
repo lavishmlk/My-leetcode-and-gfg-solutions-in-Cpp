ad807dd17d6f26aca671662383de3f496a464804
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-25 21:37:37 +0530

    Time: 7 ms (65.02%), Space: 12.3 MB (96.24%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //isko bfs se kia hua hai pichle submission me par ye dfs wli approsach hai discuss se uthai
+int findMaxLen(TreeNode* root)
+{
+    if(root==NULL) return 0;
+    return 1+max(findMaxLen(root->left),findMaxLen(root->right));
+}
+void levelOrderRec(TreeNode* root, vector<vector<int>>& vec, int level)
+{
+    if(root==NULL) return;
+      
+          //imp see how we pushed in vector see my notebook pg 17
+//vec[level].push_back({root -> val}); //domo tarike sahi hai coz 2d vector already main me bana rakha hai
+       vec[level].push_back(root -> val);
+
+    levelOrderRec(root->left,vec,level-1);
+    levelOrderRec(root->right,vec,level-1);
+}
+
+vector<vector<int> > levelOrderBottom(TreeNode *root) {
+    vector<vector<int>> result;
+    if(root==NULL) return result;
+    int count = findMaxLen(root);
+ 
+    for(int i = 0; i < count; i++)
+    {
+        vector<int> temp;
+        result.push_back(temp);
+    }
+    levelOrderRec(root, result, count-1);
+    return result;
+}
+
+};
