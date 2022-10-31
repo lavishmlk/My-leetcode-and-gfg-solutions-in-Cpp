581ea9df8cc95e60d9510ff09b92179d0ff0005b
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-31 15:44:20 +0530

    Time: 154 ms (5.01%), Space: 282.5 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    
-   bool helper(string s,string t,int index1,int index2){
+    //by my own
+   bool helper(string s,string t,int index1,int index2,vector<vector<int>> &v){
         
         if(index1==s.length()){
             return true;
@@ -9,23 +9,29 @@ public:
         if(index2==t.length()){
             return false;
         }
-        bool val=false;
+       if(v[index1][index2] != -1)
+            return v[index1][index2];
+       
        if(s[index1]==t[index2]){
-        return helper(s,t,index1+1,index2+1);
+        return v[index1][index2] =helper(s,t,index1+1,index2+1,v);
        }
         else{
-           return helper(s,t,index1,index2+1);
+           return v[index1][index2] =helper(s,t,index1,index2+1,v);
         }
     }
     
     bool isSubsequence(string s, string t) {
+            int m = s.length();
+        int n = t.length();
         if(s.length()==0){
             return true;
         }
         if(t.length()==0){
             return false;
         }
-       return helper(s,t,0,0);
+        
+         vector<vector<int>> v(m+1,vector<int> (n+1,-1));
+       return helper(s,t,0,0,v);
         
     }
 };
