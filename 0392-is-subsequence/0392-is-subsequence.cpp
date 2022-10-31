67dbef82574a2f38f9f10678802d89e700a0b2b7
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-10-31 17:55:47 +0530

    Time: 127 ms (5.01%), Space: 282.4 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -35,3 +35,51 @@ public:
         
     }
 };
+
+//BY Aditya verma video 31
+// class Solution {
+// public:
+    
+//         int lcs(string &text1, string &text2,int m,int n){
+        
+//       int t[m+1][n+1];
+//         for(int i=0;i<=m;i++)
+//         {
+//         for(int j=0;j<=n;j++){
+//                   if(i==0 || j==0){
+//             t[i][j]=0;
+//         }
+//         }
+//         }
+     
+        
+//         for(int i=1;i<=m;i++)
+//         {
+//             for(int j=1;j<=n;j++){
+//                  if(text1[i-1]==text2[j-1]){
+//           t[i][j]=1+t[i-1][j-1];
+//         }
+//         else{
+//           // t[m][n]=max(t[m-1][n],t[n][m-1]);
+//              t[i][j]=max(t[i-1][j],t[i][j-1]);
+        
+//         }
+//             }
+//         }
+//         return t[m][n];
+       
+//     }
+    
+//     bool isSubsequence(string s, string t) {
+//            int m=s.size();
+//         int n=t.size(); 
+       
+//    int lengthoflcs=lcs(s,t,m,n);
+//         if(lengthoflcs==m){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// };
