55991d382256cae1d5d252ec042d9ccc6c78458e
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-11-02 17:15:14 +0530

    Time: 6 ms (19.42%), Space: 6.3 MB (29.25%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -28,3 +28,53 @@ public:
 		return -1;
     }
 };
+
+//m2 gfg
+//similar to upper codejust different way
+// C++ program to check if a string is
+// substring of other.
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Returns true if s1 is substring of s2
+// int isSubstring(string s1, string s2)
+// {
+// 	int M = s1.length();
+// 	int N = s2.length();
+
+// 	/* A loop to slide pat[] one by one */
+// 	for (int i = 0; i <= N - M; i++) {
+// 		int j;
+
+// 		/* For current index i, check for
+// pattern match */
+// 		for (j = 0; j < M; j++)
+// 			if (s2[i + j] != s1[j])
+// 				break;
+
+// 		if (j == M)
+// 			return i;
+// 	}
+
+// 	return -1;
+// }
+
+// /* Driver code */
+// int main()
+// {
+// 	string s1 = "for";
+// 	string s2 = "geeksforgeeks";
+// 	int res = isSubstring(s1, s2);
+// 	if (res == -1)
+// 		cout << "Not present";
+// 	else
+// 		cout << "Present at index " << res;
+// 	return 0;
+// }
+
+//m3 stl for oa's
+//tc-shyd O(n)
+//bro coders-https://www.youtube.com/watch?v=AZuZZgUrQTo&list=PLjeQ9Mb66hM05uqfKysHKi3eA5HhK7QSy&index=9 -after 4:30
+
+//m4 
+//kmp
