617fa5fe3739c09da22bedc3dce9043208731927
Lavish Malik <67894036+lavishmlk@users.noreply.github.com>
2022-11-08 18:27:55 +0530

    Time: 22 ms (96.77%), Space: 13.6 MB (90.40%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    //get connected components
+    //in input adjacency matrix isgiven
+  void bfs(vector<vector<int>>& isConnected,vector<bool>&visited,int i){
+        
+        visited[i]=true;
+      for(int j=0;j<isConnected[i].size();j++){
+          if(!visited[j] && isConnected[i][j]==1){
+          bfs(isConnected,visited,j);
+          }
+      }
+        
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        
+        
+        
+        vector<bool>visited(isConnected.size(),false);
+        int count=0;
+        for(int i=0;i<isConnected.size();i++){
+         if(!visited[i]){
+               bfs(isConnected,visited,i);
+            count++;
+         }
+        }
+        return count;
+
+    }
+};
